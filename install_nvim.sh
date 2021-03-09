75f182ad4316e4a0bf52c51bd324ab35e390dba4
eomiso <ytrtef@gmail.com>
2021-03-10 01:12:40 +0900

    Update install_nvim.sh
    add vimplugins and vim-plug

--- install_nvim.sh
+++ install_nvim.sh
@@ -1,2 +1,10 @@
 curl -LO https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz
 tar xzvf nvim-linux64.tar.gz
+
+
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+
+installing "vim plugins"
+nvim -E -s -u ~/.config/nvim/init.vim +PlugInstall +qall!
