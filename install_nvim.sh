885ce90085b96c8a79db955d5da5052b1ceb99c7
eomiso <ytrtef@gmail.com>
2021-03-31 01:32:52 +0900

    Update install_nvim.sh

--- install_nvim.sh
+++ install_nvim.sh
@@ -12,15 +12,5 @@ sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.
 #install vim plugins
 installing "vim plugins"
 nvim -E -s -u ~/.config/nvim/init.vim +PlugInstall +qall!
-curl -LO https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz
-tar xzvf nvim-linux64.tar.gz
 
 # you might have to set $VIMRUNTIME to nvim-linux64/share/nvim/runtime/
-
-#install vim-plug
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-
-#install vim plugins
-installing "vim plugins"
-nvim -E -s -u ~/.config/nvim/init.vim +PlugInstall +qall!
